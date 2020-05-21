bbe11fcb668526dab5046a0c716450308ff15a81
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-05-21 16:38:03 +0300

    Fix

--- server/__init__.py
+++ server/__init__.py
@@ -26,34 +26,33 @@ def add_new_event():
     Время - строка в ISO-формате
     """
     if request.method == "POST":
-        event = {
-            "_id": get_next_id("event"),
-            "title": request.form.get("title"),
-            "author": request.form.get("author"),
-            "photo": request.form.get("photo"),
-            "start_time": datetime.fromisoformat(request.form.get("start_time")),
-            "end_time": datetime.fromisoformat(request.form.get("end_time")),
-            "description": request.form.get("description"),
-            "director": request.form.get("director"),
-            "actors": request.form.get("actors")
-        }
-        price = {
-            "1-3": float(request.form.get("price_1-3")) if request.form.get("price_1-3") != "" else 800,
-            "4-5": float(request.form.get("price_4-5")) if request.form.get("price_4-5") != "" else 1000,
-            "6-8": float(request.form.get("price_6-8")) if request.form.get("price_6-8") != "" else 1500,
-            "9-10": float(request.form.get("price_9-10")) if request.form.get("price_9-10") != "" else 1200,
-            "11-13": float(request.form.get("price_11-13")) if request.form.get("price_11-13") != "" else 900
-        }
-        color = {
-            "1-3": request.form.get("color_1-3") if request.form.get("color_1-3") != "" else "#9497ff",
-            "4-5": request.form.get("color_4-5") if request.form.get("color_4-5") != "" else "#86fcbd",
-            "6-8": request.form.get("color_6-8") if request.form.get("color_6-8") != "" else "#f0fc62",
-            "9-10": request.form.get("color_9-10") if request.form.get("color_9-10") != "" else "#ffcf70",
-            "11-13": request.form.get("color_11-13") if request.form.get("color_11-13") != "" else "#ffa570"
-        }
-
         try:
             with MONGO.cx.start_session() as session:
+                event = {
+                    "_id": get_next_id("event", session=session),
+                    "title": request.form.get("title"),
+                    "author": request.form.get("author"),
+                    "photo": request.form.get("photo"),
+                    "start_time": datetime.fromisoformat(request.form.get("start_time")),
+                    "end_time": datetime.fromisoformat(request.form.get("end_time")),
+                    "description": request.form.get("description"),
+                    "director": request.form.get("director"),
+                    "actors": request.form.get("actors")
+                }
+                price = {
+                    "1-3": float(request.form.get("price_1-3")) if request.form.get("price_1-3") != "" else 800,
+                    "4-5": float(request.form.get("price_4-5")) if request.form.get("price_4-5") != "" else 1000,
+                    "6-8": float(request.form.get("price_6-8")) if request.form.get("price_6-8") != "" else 1500,
+                    "9-10": float(request.form.get("price_9-10")) if request.form.get("price_9-10") != "" else 1200,
+                    "11-13": float(request.form.get("price_11-13")) if request.form.get("price_11-13") != "" else 900
+                }
+                color = {
+                    "1-3": request.form.get("color_1-3") if request.form.get("color_1-3") != "" else "#9497ff",
+                    "4-5": request.form.get("color_4-5") if request.form.get("color_4-5") != "" else "#86fcbd",
+                    "6-8": request.form.get("color_6-8") if request.form.get("color_6-8") != "" else "#f0fc62",
+                    "9-10": request.form.get("color_9-10") if request.form.get("color_9-10") != "" else "#ffcf70",
+                    "11-13": request.form.get("color_11-13") if request.form.get("color_11-13") != "" else "#ffa570"
+                }        
                 txn_add_event(session, event, price, color)
         except ErrorDataDB as error_db:
             return jsonify({"message": error_db.message, "is_success": False})
@@ -64,8 +63,8 @@ def add_new_event():
 
 @run_transaction_with_retry
 def txn_add_event(session, event, price, color):
-    event["_id"] = MONGO.db.event.insert_one(event).inserted_id        
-    add_tickets(event["_id"], price, color)
+    event["_id"] = MONGO.db.event.insert_one(event, session=session).inserted_id        
+    add_tickets(event["_id"], price, color, session=session)
     commit_with_retry(session)
     return True
 
@@ -83,12 +82,12 @@ def get_next_id(collection_name, session=None):
     return ObjectId(str(result["count"]).zfill(24))
 
 
-def add_tickets(id_event, price, color):
+def add_tickets(id_event, price, color, session=None):
     """ Добавление билетов """
     for i in range(1, 14):
         for j in range(1, 22):
             ticket = {
-                "_id": get_next_id("ticket"),
+                "_id": get_next_id("ticket", session=session),
                 "row": i,
                 "seat": j,
                 "event": id_event,
@@ -109,7 +108,7 @@ def add_tickets(id_event, price, color):
             else:
                 ticket["price"] = price["11-13"]
                 ticket["color_zone"] = color["11-13"]
-            ticket["_id"] = MONGO.db.ticket.insert_one(ticket).inserted_id
+            ticket["_id"] = MONGO.db.ticket.insert_one(ticket, session=session).inserted_id
 
 
 # @APP.route("/events_list")

fef9042bec6476043dc612978e1490f4c082d33e
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-05-23 17:55:12 +0300

    Add a daemon

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,7 @@
 """ Билетная касса театра """
 import os
 from datetime import datetime
+import threading
 
 from bson import ObjectId
 from flask import Flask, jsonify, request, send_from_directory
@@ -27,38 +28,54 @@ def add_new_event():
     """
     if request.method == "POST":
         try:
-            with MONGO.cx.start_session() as session:
-                event = {
-                    "title": request.form.get("title"),
-                    "author": request.form.get("author"),
-                    "photo": request.form.get("photo"),
-                    "start_time": datetime.fromisoformat(request.form.get("start_time")),
-                    "end_time": datetime.fromisoformat(request.form.get("end_time")),
-                    "description": request.form.get("description"),
-                    "director": request.form.get("director"),
-                    "actors": request.form.get("actors")
-                }
-                price = {
-                    "1-3": float(request.form.get("price_1-3")) if request.form.get("price_1-3") != "" else 800,
-                    "4-5": float(request.form.get("price_4-5")) if request.form.get("price_4-5") != "" else 1000,
-                    "6-8": float(request.form.get("price_6-8")) if request.form.get("price_6-8") != "" else 1500,
-                    "9-10": float(request.form.get("price_9-10")) if request.form.get("price_9-10") != "" else 1200,
-                    "11-13": float(request.form.get("price_11-13")) if request.form.get("price_11-13") != "" else 900
-                }
-                color = {
-                    "1-3": request.form.get("color_1-3") if request.form.get("color_1-3") != "" else "#9497ff",
-                    "4-5": request.form.get("color_4-5") if request.form.get("color_4-5") != "" else "#86fcbd",
-                    "6-8": request.form.get("color_6-8") if request.form.get("color_6-8") != "" else "#f0fc62",
-                    "9-10": request.form.get("color_9-10") if request.form.get("color_9-10") != "" else "#ffcf70",
-                    "11-13": request.form.get("color_11-13") if request.form.get("color_11-13") != "" else "#ffa570"
-                }        
-                txn_add_event(session, event, price, color)
+            event, price, color = parse_data()        
+            thread = threading.Thread(target=add_event_data, args=(event, price, color))
+            thread.daemon = True
+            thread.start()
         except ErrorDataDB as error_db:
-            return jsonify({"message": error_db.message, "is_success": False})
+            return jsonify({"message": error_db.message})            
+        except Exception:
+            return jsonify({"message": "Ошибка при добавлении данных"}) 
 
-        return jsonify("Save!")
+        return jsonify({"message": "Данные добавляются в фоновом режиме. Это занимает пару минут"})
 
     return send_from_directory(APP.static_folder, "index.html")
+
+def parse_data():
+    try:
+        event = {
+            "title": request.form.get("title"),
+            "author": request.form.get("author"),
+            "photo": request.form.get("photo"),
+            "start_time": datetime.fromisoformat(request.form.get("start_time")),
+            "end_time": datetime.fromisoformat(request.form.get("end_time")),
+            "description": request.form.get("description"),
+            "director": request.form.get("director"),
+            "actors": request.form.get("actors")
+        }
+        price = {
+            "1-3": float(request.form.get("price_1-3")) if request.form.get("price_1-3") != "" else 800,
+            "4-5": float(request.form.get("price_4-5")) if request.form.get("price_4-5") != "" else 1000,
+            "6-8": float(request.form.get("price_6-8")) if request.form.get("price_6-8") != "" else 1500,
+            "9-10": float(request.form.get("price_9-10")) if request.form.get("price_9-10") != "" else 1200,
+            "11-13": float(request.form.get("price_11-13")) if request.form.get("price_11-13") != "" else 900
+        }
+        color = {
+            "1-3": request.form.get("color_1-3") if request.form.get("color_1-3") != "" else "#9497ff",
+            "4-5": request.form.get("color_4-5") if request.form.get("color_4-5") != "" else "#86fcbd",
+            "6-8": request.form.get("color_6-8") if request.form.get("color_6-8") != "" else "#f0fc62",
+            "9-10": request.form.get("color_9-10") if request.form.get("color_9-10") != "" else "#ffcf70",
+            "11-13": request.form.get("color_11-13") if request.form.get("color_11-13") != "" else "#ffa570"
+        }
+    except Exception:
+        raise ErrorDataDB("Ошибка при парсинге данных") 
+    return event, price, color     
+
+
+def add_event_data(event, price, color):
+    with MONGO.cx.start_session() as session:    
+        txn_add_event(session, event, price, color)
+
 
 @run_transaction_with_retry
 def txn_add_event(session, event, price, color):
